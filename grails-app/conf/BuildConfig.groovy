136483f363b863513d46b3a947787ac8252b0fa1
Omasiri Udeinya <omasiri@novadge.com>
2014-04-15 11:51:16 +0100

    BuildConfig cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -18,20 +18,20 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3',":tomcat:7.0.50.1", {
+        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
             export = false
         }
-       
-       
-        // plugins for the compile step -- remember to remove
-        build ":tomcat:7.0.50.1"
-        compile ":scaffolding:2.0.2"
-        compile ':cache:1.1.1'
-        
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
-        runtime ":database-migration:1.3.8"
-        
-        runtime ":resources:1.2.1"
+//       
+//       
+//        // plugins for the compile step -- remember to remove
+//        build ":tomcat:7.0.50.1"
+//        compile ":scaffolding:2.0.2"
+//        compile ':cache:1.1.1'
+//        
+//        // plugins needed at runtime but not for compilation
+//        runtime ":hibernate:3.6.10.8" // or ":hibernate4:4.3.1.1"
+//        runtime ":database-migration:1.3.8"
+//        
+//        runtime ":resources:1.2.1"
     }
 }

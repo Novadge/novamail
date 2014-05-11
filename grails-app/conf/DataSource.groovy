8c1b85248c0b57506a527990345c08c80b4651d2
Omasiri Udeinya <omasiri@novadge.com>
2014-05-11 10:37:48 +0100

    cleanup

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,16 +1,1 @@
-dataSource {
-    pooled = true
-    driverClassName ="com.mysql.jdbc.Driver"// "org.h2.Driver"
-    username = "novadge"
-    password = "\$Money38"
-    dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-    dbCreate = 'update'
-   // url = 'jdbc:h2:mem:testDb'
-    url = "jdbc:mysql://localhost:3306/novamail"
-}
 
-hibernate {
-    cache.use_second_level_cache = false
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
-}

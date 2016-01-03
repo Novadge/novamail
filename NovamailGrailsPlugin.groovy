29c7f5479b45ca26dcf3957825a8842fef9ff6d3
Joseph Udeinya <omasiri@novadge.com>
2016-01-03 13:37:38 +0100

    version number update

--- NovamailGrailsPlugin.groovy
+++ NovamailGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class NovamailGrailsPlugin {
     // the plugin version
-    def version = "0.3.8"
+    def version = "0.3.9"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging

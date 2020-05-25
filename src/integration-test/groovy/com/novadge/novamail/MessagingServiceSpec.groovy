2418fa638614f1d071931788291d12c54abff642
Omasirichukwu Udeinya <omasiri@gmail.com>
2020-05-24 23:01:33 -0400

    upgraded plugin go grails 4

---
+++ src/integration-test/groovy/com/novadge/novamail/MessagingServiceSpec.groovy
@@ -0,0 +1,35 @@
+package com.novadge.novamail
+
+
+import spock.lang.Specification
+import org.springframework.beans.factory.annotation.Value
+
+import grails.core.GrailsApplication
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+
+class MessagingServiceSpec extends Specification {
+
+
+    static doWithConfig(c) {
+        
+    }
+
+    def setup() {
+
+    }
+
+    def cleanup() {
+    }
+
+    void "test sendEmail(map.hostname, map.username, map.password, map.username, map.to, map.cc,map.bcc, map.subject, map.body,html, map.attachments,hostProps) "(){
+
+        
+    }
+
+
+    
+}
+
+
--- src/main/groovy/novamail/NovamailGrailsPlugin.groovy
+++ src/main/groovy/novamail/NovamailGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class NovamailGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.1.1 > *"
+    def grailsVersion = "4.0.3 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
